329807c66a6c8aec4039af6662eb9bf040a2a8f3
David Berger <david.berger@kitware.com>
2022-08-31 22:27:12 +0200

    fix(config): Fix missing .png files of markers and layers

--- vue-components/vue.config.js
+++ vue-components/vue.config.js
@@ -1,7 +1,21 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 const DST_PATH = '../trame_leaflet/module/serve';
 
 module.exports = {
   outputDir: path.resolve(__dirname, DST_PATH),
   publicPath: './',
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: 'node_modules/leaflet/dist/images/marker-icon.png', to: "tramemarker-icon.png" },
+          { from: 'node_modules/leaflet/dist/images/marker-icon-2x.png', to: "tramemarker-icon-2x.png" },
+          { from: 'node_modules/leaflet/dist/images/marker-shadow.png', to: "tramemarker-shadow.png" },
+          { from: 'node_modules/leaflet/dist/images/layers.png', to: "tramelayers.png" },
+          { from: 'node_modules/leaflet/dist/images/layers-2x.png', to: "tramelayers-2x.png" },
+        ],
+      }),
+    ],
+  },
 };
